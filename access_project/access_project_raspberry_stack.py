ffe9821be62d8a001ff6fe9ae5cfdb87e8ad7a40
MerviGit <mervi.siipilahde@gmail.com>
2020-09-18 15:50:14 +0300

    raspberry stack added to project

---
+++ access_project/access_project_raspberry_stack.py
@@ -0,0 +1,9 @@
+from aws_cdk import (
+    core
+)
+
+
+class RaspberryStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from aws_cdk import core
 
 from access_project.access_project_stack import AccessProjectStack
 from access_project.access_project_database_stack import DatabaseStack
+from access_project.access_project_raspberry_stack import RaspberryStack
 
 
 app = core.App()
 AccessProjectStack(app, "access-project")
 DatabaseStack(app, "access-project-database")
+RaspberryStack(app, "access-project-raspberry")
 
 app.synth()
