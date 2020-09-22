c6099631be40fd39d2d4dffc7e333db2cb1e3265
MerviGit <mervi.siipilahde@gmail.com>
2020-09-22 11:00:40 +0300

    lisätty raspberry_pi_user

--- access_project/access_project_raspberry_stack.py
+++ access_project/access_project_raspberry_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import (
+    aws_iam as iam,
     core
 )
 
@@ -7,3 +8,10 @@ class RaspberryStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        rasberry_pi_user = iam.User(
+            self, "RaspberryPiUser",
+            managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodeCommitPowerUser"),
+                              iam.ManagedPolicy.from_aws_managed_policy_name("AWSIoTFullAccess"),
+                              iam.ManagedPolicy.from_aws_managed_policy_name("RaspberryPiWriteS3AccessProjectBuckets")]
+        )
