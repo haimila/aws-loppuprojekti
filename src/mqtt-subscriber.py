2ca2d72cbf49853989dcc7d7b2f66a0c29165c8b
mitja <kickel@smor.se>
2020-09-22 09:07:08 +0000

    Squashed commit of the following
    commit c25fb411 
    Author: MitjaHaimila <mitja.haimila@gmail.com> 
    Date: Tue Sep 22 2020 12:04:30 GMT+0300 (Eastern European Summer Time) 
    
        lisätty mqtt skripti

---
+++ src/mqtt-subscriber.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
+    print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
+    client.subscribe("raspberry/accesstopic")  # Subscribe to the topic”, receive any messages published on it
+
+
+def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
+    print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
+
+
+client = mqtt.Client("raspberry")  # Create instance of client with client ID “digi_mqtt_test”
+client.on_connect = on_connect  # Define callback function for successful connection
+client.on_message = on_message  # Define callback function for receipt of a message
+
+client.connect('a32uqpa8m3tpx7-ats.iot.us-east-1.amazonaws.com')
+client.loop_forever()  # Start networking daemon
+
