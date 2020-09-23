3fff02323a966c58d80f4f18127d2751e2c59677
MitjaHaimila <mitja.haimila@gmail.com>
2020-09-23 12:32:45 +0300

    lisätty stream delete to s3

---
+++ lambda/stream_delete_event_to_s3.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('rasberry-bucket')
+
+
+def stream_delete_event_to_s3(event, context):
+    try:
+        userid = event['Records'][0]['dynamodb']['Keys']['id']['S']
+
+        if event['Records'][0]['eventName'] == 'REMOVE':
+
+            bucket.delete_objects(
+                Delete={
+                    'Objects': [
+                        {
+                            'Key': f'{userid}.jpg'
+                        }]})
+
+        else:
+            pass
+
+    except:
+        pass
+
+    return {"message": f"user {userid} deleted"}
