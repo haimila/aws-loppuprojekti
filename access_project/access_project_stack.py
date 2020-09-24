06fa409406677777cd7deff8f2a427a1c012811b
MitjaHaimila <mitja.haimila@gmail.com>
2020-09-24 16:52:18 +0300

    testings

--- access_project/access_project_stack.py
+++ access_project/access_project_stack.py
@@ -535,5 +535,5 @@ class AccessProjectStack(core.Stack):
             code=_lambda.Code.asset('lambda'),
             handler='start_dbcheck_state_machine.start_state_machine',
             initial_policy=[start_state_machine_policy_statement],
-            environment={"state_machine": f"arn:aws:states:{region}:{accountid}:stateMachine:{state_machine.state_machine_name}"}
+            environment={"state_machine": "arn:aws:states:%s:%s:stateMachine:%s" % (region, accountid, state_machine.state_machine_name)}
         )
