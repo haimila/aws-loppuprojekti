ef108dbbcc7232a13d8536cba3c317ffae258005
MitjaHaimila <mitja.haimila@gmail.com>
2020-09-25 13:01:09 +0300

    muokattu initial authenticationia

--- lambda/evaluate_initial_authentication.py
+++ lambda/evaluate_initial_authentication.py
@@ -1,5 +1,5 @@
 def evaluate_initial_authentication(event, context):
-    if event[0]['user']['id'] == 'unknown' or event[1]['face'] == 'notavailable' or event[1]['face'] == 'unknown':
+    if event[0]['user'] == 'unknown' or event[1]['face'] == 'notavailable' or event[1]['face'] == 'unknown':
         response = {
             "user": event[0]["user"],
             "state": "failed"
